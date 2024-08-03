0eaa5700257a8084a3e6c967ec81e5582f4aa15a
mcicerani <mcicerani@gmail.com>
2024-08-03 18:08:31 +0200

    fix render

--- admin.js
+++ admin.js
@@ -1,51 +1,40 @@
-const {default: build} = await import("https://esm.sh/build")
-
-const mod = await build({
-  dependencies: {
-    "sanity": "^3.27.0",
-    "@sanity/vision": "^3.27.0",
-  },
-  source: `
-      import { defineConfig, renderStudio } from "sanity";
-      import { structureTool } from "sanity/structure";
-      import {presentationTool} from 'sanity/presentation';
-      import { visionTool } from "@sanity/vision";
-
-      const config = defineConfig({
-        basePath: '/admin',
-        projectId: "50cvvnf6",
-        dataset: "production",
-        schema: {
-          types: [
-            {
-              type: "document",
-              name: "post",
-              title: "Post",
-              fields: [
-                {
-                  type: "string",
-                  name: "title",
-                  title: "Title"
-                }
-              ]
-            }
-          ]
-        },
-        plugins: [structureTool(), presentationTool({}), visionTool()]
-      });
-const div = document.createElement('div')
-document.body.innerHTML = ''
-document.body.appendChild(div)
-export const render = () => renderStudio(div, config);
-  `,
-  // for types checking and LSP completion
-  types: `
-    export function render(): string;
-  `,
-});
-
-// import module
-const { render } = await import(mod.bundleUrl);
-
-render()
-
+(async () => {
+    const { default: build } = await import("https://esm.sh/build");
+  
+    const mod = await build({
+      dependencies: {
+        "sanity": "^3.27.0",
+        "@sanity/vision": "^3.27.0",
+      },
+      source: `
+        import { defineConfig, renderStudio } from "sanity";
+        import { structureTool } from "sanity/structure";
+        import { presentationTool } from 'sanity/presentation';
+        import { visionTool } from "@sanity/vision";
+  
+        // Schemi importati
+        import { schemaTypes } from 'my-website/schemasType/index.js'; // Assicurati che il percorso sia corretto
+  
+        const config = defineConfig({
+          basePath: '/admin',
+          projectId: "50cvvnf6", // Sostituisci con il tuo projectId
+          dataset: "production", // Sostituisci con il tuo dataset
+          schema: {
+            types: schemaTypes
+          },
+          plugins: [structureTool(), presentationTool({}), visionTool()]
+        });
+        const div = document.getElementById('studio-container');
+        export const render = () => renderStudio(div, config);
+      `,
+      // per il controllo dei tipi e il completamento LSP
+      types: `
+        export function render(): string;
+      `,
+    });
+  
+    // Importa ed esegui il modulo
+    const { render } = await import(mod.bundleUrl);
+    render();
+  })();
+  
